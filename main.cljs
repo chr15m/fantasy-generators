5fb12166e84a22506050b9a208f1241d31c85bfe
Chris McCormick <chris@mccormick.cx>
2025-03-14 15:48:39 +0800

    Initial design with basic tavern name generator.

---
+++ main.cljs
@@ -0,0 +1,35 @@
+(ns main
+  (:require
+    [reagent.core :as r]
+    [reagent.dom :as rdom]))
+
+(defonce state (r/atom {:selected nil}))
+
+(defn card [title image-url link]
+  [:div.card 
+   {:on-click #(js/window.location.assign link)
+    :class (when (= title (:selected @state)) "selected")}
+   [:div.card-image [:img {:src image-url :alt title}]]
+   [:div.card-title title]])
+
+(defn app []
+  [:main
+   [:header
+    [:div.title "Fantasy Generator"]
+    [:nav
+     [:a {:href "#"} "News"]
+     [:a {:href "#"} "FAQ"]]]
+   
+   [:h1 "Pick a generator..."]
+   
+   [:div.cards-container
+    [card "TAVERN NAME" "https://placehold.co/300x300/e2e2d5/333?text=Tavern" "tavern-name-generator"]
+    [card "CITY" "https://placehold.co/300x300/f5f5dc/333?text=City" "#"]
+    [card "VILLAGE" "https://placehold.co/300x300/c8e6c9/333?text=Village" "#"]
+    [card "DUNGEON" "https://placehold.co/300x300/e0e0e0/333?text=Dungeon" "#"]
+    [card "CHARACTER" "https://placehold.co/300x300/d7ccc8/333?text=Character" "#"]
+    [card "QUEST" "https://placehold.co/300x300/ffe0b2/333?text=Quest" "#"]]
+   
+   [:footer "© 2025 Fantasy Generator"]])
+
+(rdom/render [app] (.getElementById js/document "app"))
